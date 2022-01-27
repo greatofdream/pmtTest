06f8220ea1044845ea72fcbc934de9e1aafce847
greatofdream <greatofdream@gmail.com>
2022-01-27 15:31:26 +0000

    add the style; adjust the bin for pulse

--- trigger/preview.py
+++ trigger/preview.py
@@ -2,6 +2,7 @@ import uproot, numpy as np, h5py
 import argparse
 from pandas import Series
 import matplotlib.pyplot as plt
+plt.style.use('presentation.mpldstyle')
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 from matplotlib.backends.backend_pdf import PdfPages
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
@@ -11,8 +12,6 @@ psr.add_argument('-i', dest='ipt', help='input root file')
 psr.add_argument('-o', dest='opt', help='output pdf')
 psr.add_argument('-c', dest='channel', nargs='+', default=[0,1],help='channel used in DAQ')
 psr.add_argument('-s', dest='nsigma', default=5, type=float, help='nsimga')
-psr.add_argument('-n', dest='nchannel', type=int, default=8, help='number of channel')
-psr.add_argument('-l', dest='waveCut', type=int, default=0, help='cut of wave length')
 psr.add_argument('-e', dest='eventid', type=int, default=-1, help='eventid')
 psr.add_argument('-t', dest='trigger', default=1, help="channel id of trigger")
 args = psr.parse_args()
@@ -25,23 +24,25 @@ with uproot.open(args.ipt) as ipt:
 eventNum = waveforms.shape[0]
 chmap = Series(range(channelIds[0].shape[0]), index = channelIds[0])
 chindex = chmap.loc[chs]
-assert(1)
 nch = len(channelIds[0])
+wavelength = waveforms[0].shape[0]//nch
+maxcut = min(wavelength, 1000)
 if args.eventid<0:
     with PdfPages('{}'.format(args.opt)) as pdf:
-        for eid in range(0,np.min([100,eventNum])):
+        for eid in range(0, np.min([100,eventNum])):
             fig, ax = plt.subplots(figsize=(10, 5))
             ax.set_title('eid {}'.format(eventid[eid]))
             ax.set_xlabel('t/ns')
             ax.set_ylabel('Amplitude/mV')
-            ax.plot(waveforms[eid].reshape((nch,-1))[chindex, :].T, label=['ch{}'.format(i) for i in chs])
-            axins = inset_axes(ax, width="40%", height="30%",loc='lower left',
+            ax.plot(waveforms[eid].reshape((nch, -1))[chindex, :].T, label=['ch{}'.format(i) for i in chs])
+            axins = inset_axes(ax, width="40%", height="30%", loc='lower left',
                    bbox_to_anchor=(0.2, 0.1, 1, 2),
                    bbox_transform=ax.transAxes)
-            axins.plot(waveforms[eid].reshape((nch,-1))[chindex,:1000].T,label=['ch{}'.format(i) for i in chs])
-            axins.set_ylim([900,1000])
+            axins.plot(waveforms[eid].reshape((nch,-1))[chindex, :maxcut].T, label=['ch{}'.format(i) for i in chs])
+            axins.set_ylim([900, 960])
             mark_inset(ax, axins, loc1=3, loc2=1, fc="none", ec='k', lw=1)
             ax.legend()
+            fig.tight_layout()
             pdf.savefig()
             plt.close(fig)
 
--- trigger/pulseRatio.py
+++ trigger/pulseRatio.py
@@ -2,6 +2,8 @@ import numpy as np, h5py
 import argparse
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+plt.style.use('presentation.mplstyle')
+
 if __name__=="__main__":
     psr = argparse.ArgumentParser()
     psr.add_argument('-i', dest='ipt', help='input h5 file')
@@ -28,28 +30,34 @@ if __name__=="__main__":
     delay1_ratio_ch3 = len(np.where((delaypul1_ch3>-1))[0])/np.sum(trig_ch3)
     delay10_ratio_ch3 = len(np.where((delaypul10_ch3>-1))[0])/np.sum(trig_ch3)
     with PdfPages(args.opt) as pdf:
-        fig, ax = plt.subplots()
-        ax.hist(prompt_ch2[prompt_ch2>-1], range=[100, 300], bins=50, histtype='step', label='ch2')
-        ax.hist(prompt_ch3[prompt_ch3>-1], range=[100, 300], bins=50, histtype='step', label='ch3')
-        ax.set_yscale('log')
-        ax.set_xlabel('pulsePosition[ns]')
+        fig, ax = plt.subplots(dpi=150)
+        ax.hist(prompt_ch2[prompt_ch2>-1], range=[100, 300], bins=30, histtype='step', label='ch2')
+        ax.hist(prompt_ch3[prompt_ch3>-1], range=[100, 300], bins=30, histtype='step', label='ch3')
+        # ax.set_yscale('log')
+        ax.set_xlabel('pulsePosition/ns')
         ax.set_ylabel('entries')
+        ax.legend()
+        fig.tight_layout()
         pdf.savefig(fig)
 
-        fig, ax = plt.subplots()
-        ax.hist(delaypul1_ch2[delaypul1_ch2>-1], range=[600, 1300], bins=700, histtype='step', label='ch2')
-        ax.hist(delaypul1_ch3[delaypul1_ch3>-1], range=[600, 1300], bins=700, histtype='step', label='ch3')
-        ax.set_yscale('log')
-        ax.set_xlabel('pulsePosition[ns]')
+        fig, ax = plt.subplots(dpi=150)
+        ax.hist(delaypul1_ch2[delaypul1_ch2>-1], range=[600, 1300], bins=70, histtype='step', label='ch2')
+        ax.hist(delaypul1_ch3[delaypul1_ch3>-1], range=[600, 1300], bins=70, histtype='step', label='ch3')
+        # ax.set_yscale('log')
+        ax.set_xlabel('pulsePosition/ns')
         ax.set_ylabel('entries')
+        ax.legend()
+        fig.tight_layout()
         pdf.savefig(fig)
 
-        fig, ax = plt.subplots()
-        ax.hist(delaypul10_ch2[delaypul10_ch2>-1], range=[1300, 10300], bins=10000, histtype='step', label='ch2')
-        ax.hist(delaypul10_ch3[delaypul10_ch3>-1], range=[1300, 10300], bins=10000, histtype='step', label='ch3')
-        ax.set_yscale('log')
-        ax.set_xlabel('pulsePosition[ns]')
+        fig, ax = plt.subplots(dpi=150)
+        ax.hist(delaypul10_ch2[delaypul10_ch2>-1], range=[1300, 10300], bins=500, histtype='step', label='ch2')
+        ax.hist(delaypul10_ch3[delaypul10_ch3>-1], range=[1300, 10300], bins=500, histtype='step', label='ch3')
+        # ax.set_yscale('log')
+        ax.set_xlabel('pulsePosition/ns')
         ax.set_ylabel('entries')
+        ax.legend()
+        fig.tight_layout()
         pdf.savefig(fig)
 
     print(prompt_ratio_ch2, delay1_ratio_ch2, delay10_ratio_ch2, np.sum(trig_ch2))
