2562d88edf9d413967c5923db9903893f9581736
greatofdream <greatofdream@gmail.com>
2022-05-01 22:04:33 +0800

    加入激光精确触发切割的分析

--- README.md
+++ README.md
@@ -14,9 +14,9 @@
     + 目前选择单光电子峰附近的[-5,+5]mVns区间的波形进行平均；此外如果所有波形没有信号，上面的`spePrePlot.py`给出的单光电子峰会出现错误，因此额外加上大于30mVns的阈值限制
 + 高级参数测量流程：分析激光触发后的时间窗(基础参数测量给出)->根据窗口选择出触发后的波形参数
   + `Charge`,`Gain`,`P/V`,`Resolution`,`DE`,`TR`,`TF`,`TH`,`Prompt pulse`, `Delay pulse`,`TTS`
-  + `TriggerPreAna.py`:考虑触发时间窗后，进行预分析
+  + `TriggerPreAna.py`:考虑触发时间窗后，进行预分析，为了加速，使用了600ns的长度对波形做了切割，所以这里在调整激光的上升沿时，要保证触发的信号在600ns以内，或者调整这个大小。
   + `summary.py`： 处理出触发后的时间窗区域
-  + `triggerPulse.py`: 根据触发后的时间窗区域重新分析是否有触发 
+  + `TriggerAna.py`: 根据触发后的时间窗区域重新分析是否有触发 
   + `pulseRatio.py`:分析前后脉冲比例 `pulseRatioWOnoise.py`:去除暗噪声计数的结果
   + `QE.py`: 分析触发比例
 ## Example
@@ -27,6 +27,10 @@
 + 激光触发分析run680, channels 2和3， 触发通道1;根据需要调整需要的核数，比如16核`-j16`
   ```
   make anaNum=680 channels="2 3" triggerch=1 -f Makefiles/Trigger/Makefile -j16
+  ```
++ 预览激光触发数据
+  ```
+  make preview anaNum=680 channels="2 3" triggerch=1 -f Makefiles/Trigger/Makefile
   ```
 + `QE`测量依赖于两次的交换测量，需要在task中创建一个`xxx/config.json`，执行
   ```
---
+++ TriggerAna.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+import uproot, numpy as np, h5py
+from pandas import Series
+from waveana.waveana import Qb, Qe
+import argparse
+import matplotlib.pyplot as plt
+
+promptB = 250
+promptE = 50
+delay1B = 300
+delay1E = 1000
+delay10B = 1000
+delay10E = 10000
+def Pulse(wave, base, std, up10, down10, promptB,promptE,delay1B,delay1E,delay10B,delay10E):
+    before = wave[max(0,up10-promptB):(up10-promptE)]
+    after_1 = wave[(down10+delay1B):min(down10+delay1E,wave.shape[0])]
+    after_10 = wave[(down10+delay10B):min(down10+delay10E,wave.shape[0])]
+    before_pulse = -1
+    after_1_pulse = -1
+    after_10_pulse = -1
+    before_pulse_H,after_1_pulse_H, after_10_pulse_H = 0, 0, 0
+    threshold = 5*std
+    if threshold<3:
+        threshold = 3
+    if min(before)<=(base-threshold):
+        before_pulse = np.argmin(before)+max(0,up10-promptB)
+        before_pulse_H = -np.min(before)
+    if min(after_1)<=(base-threshold):
+        after_1_pulse = np.argmin(after_1)+(down10+delay1B)
+        after_1_pulse_H = -np.min(after_1)
+    if min(after_10)<=(base-threshold):
+        after_10_pulse = np.argmin(after_10)+(down10+delay10B)
+        after_10_pulse_H = -np.min(after_10)
+    return before_pulse, after_1_pulse, after_10_pulse, before_pulse_H,after_1_pulse_H, after_10_pulse_H
+if __name__=="__main__":
+    psr = argparse.ArgumentParser()
+    psr.add_argument('-i', dest='ipt', help='input root file')
+    psr.add_argument('-o', dest='opt', help='output h5 file')
+    psr.add_argument('-c', dest='channel', nargs='+', default=[0,1],help='channel used in DAQ')
+    psr.add_argument('-t', dest='trigger', default=0, type=int, help='trigger channel')
+    psr.add_argument('-s', dest='nsigma', default=5, type=float, help='nsimga')
+    psr.add_argument('-n', dest='nchannel', type=int, default=8, help='number of channel')
+    psr.add_argument('--interval', help='the time interval for each channel')
+    psr.add_argument('--result', help='result of preanalysis')
+    psr.add_argument('--QE', default=False, action='store_true')
+    args = psr.parse_args()
+
+    triggerch = args.trigger
+    with uproot.open(args.ipt) as ipt:
+        eventIds = ipt["Readout/TriggerNo"].array(library='np')
+        waveforms = ipt["Readout/Waveform"].array(library='np')
+        channelIds = ipt["Readout/ChannelId"].array(library='np')
+
+    entries = waveforms.shape[0]
+    channels = [int(c) for c in args.channel]
+    # read the time interval
+    with h5py.File(args.interval, 'r') as ipt:
+        interval = ipt['interval'][:]
+        if args.QE:
+            rinterval = ipt['rinterval'][:]
+    # read the analysis result
+    info = []
+    with h5py.File(args.result, 'r') as ipt:
+        waveformLength = ipt.attrs['waveformLength']
+        for j in range(len(args.channel)):
+            info.append(ipt['ch{}'.format(args.channel[j])][:])
+        trigger = ipt['trigger'][:]
+    if not args.QE:
+        # inital the result
+        pulse = []
+        storedtype = [('EventID', '<i4'), ('isTrigger', bool), ('promptPulse','<f4'), ('DelayPulse1','<f4'), ('DelayPulse10','<f4'), ('promptPulseH','<f4'), ('DelayPulse1H','<f4'), ('DelayPulse10H','<f4'), ('up10', '<f4'), ('down10', '<f4')]
+        for i in range(len(args.channel)):
+            pulse.append(np.zeros((entries,),dtype=storedtype))
+
+        for i, (wave, eid, ch) in enumerate(zip(waveforms, eventIds, channelIds)):
+            wave = wave.reshape((ch.shape[0],-1))
+            chmap = Series(range(ch.shape[0]), index=ch)
+            for j in range(len(args.channel)):
+                w = wave[chmap.loc[channels[j]]]
+                anar = info[j][i]
+                baseline = anar['baseline']
+                std = anar['std']
+                threshold = args.nsigma*std
+                if threshold<3:
+                    threshold = 3
+                interval_j = [int(ti) for ti in interval[j][['start', 'end']]]
+                promptPulse, delayPulse1, delayPulse10 = -1, -1, -1
+                promptPulseH, delayPulse1H, delayPulse10H = 0, 0, 0
+                # Baseline inteval_j
+                if np.max(baseline - w[interval_j[0]:interval_j[1]])>threshold:
+                    minIndex = interval_j[0] + np.argmin(w[interval_j[0]:interval_j[1]])
+                    # rising edge 0.1 down edge 0.1
+                    up10, _, _ = Qb(w-baseline, minIndex, 0)
+                    down10, _, _ = Qe(w-baseline, minIndex, 0)
+                    # TODO:baseline-3*std
+                    # TODO:0.3-1us. 1-10us.
+                    #check if pos_histmax_'ch{}'.format(args.channel[j]) is right.it means an int number 
+                    promptPulse, delayPulse1, delayPulse10, promptPulseH, delayPulse1H, delayPulse10H = Pulse(w-baseline, 0, std, int(up10), int(down10), promptB,promptE,delay1B,delay1E,delay10B,delay10E)
+                    pulse[j][i] = (eid, True, promptPulse, delayPulse1, delayPulse10, promptPulseH, delayPulse1H, delayPulse10H, up10, down10)
+                else:
+                    pulse[j][i] = (eid, False, promptPulse, delayPulse1, delayPulse10, promptPulseH, delayPulse1H, delayPulse10H, 0, 0)
+    else:
+        pulse = []
+        storedtype = [('EventID', '<i4'), ('isTrigger', bool), ('up10', '<f4'), ('down10', '<f4'), ('minPeak', '<f4'), ('minPeakPos', '<i4'), ('charge', '<f4')]
+        for i in range(len(args.channel)):
+            pulse.append(np.zeros((entries,), dtype=storedtype))
+        for i, (wave, eid, ch) in enumerate(zip(waveforms, eventIds, channelIds)):
+            wave = wave.reshape((ch.shape[0],-1))
+            chmap = Series(range(ch.shape[0]), index=ch)
+            for j in range(len(args.channel)):
+                w = wave[chmap.loc[channels[j]]]
+                anar = info[j][i]
+                baseline = anar['baseline']
+                std = anar['std']
+                threshold = args.nsigma*std
+                if threshold<3:
+                    threshold = 3
+                interval_j = [int(ti+trigger[i]['triggerTime']) for ti in rinterval[j][['start', 'end']]]
+                # Baseline inteval_j
+                if np.max(baseline - w[interval_j[0]:interval_j[1]])>threshold:
+                    isTrigger = True
+                    rminIndex = interval_j[0] + np.argmin(w[interval_j[0]:interval_j[1]]) - int(trigger[i]['triggerTime'])
+                    # rising edge 0.1 down edge 0.1
+                    up10, _, _ = Qb(w-baseline, rminIndex + int(trigger[i]['triggerTime']), 0)
+                    down10, _, _ = Qe(w-baseline, rminIndex + int(trigger[i]['triggerTime']), 0)
+                    pulse[j][i] = (eid, isTrigger, up10, down10, baseline - min(w[interval_j[0]:interval_j[1]]), rminIndex, np.sum(baseline - w[int(up10):int(down10)]))
+                else:
+                    isTrigger = False
+                    rminIndex = interval_j[0] + np.argmin(w[interval_j[0]:interval_j[1]]) - int(trigger[i]['triggerTime'])
+                    pulse[j][i] = (eid, isTrigger, -1, -1, baseline - min(w[interval_j[0]:interval_j[1]]), rminIndex, 0)
+    with h5py.File(args.opt, 'w') as opt:
+        for j in range(len(args.channel)):
+            opt.create_dataset('ch{}'.format(args.channel[j]), data=pulse[j], compression='gzip')
