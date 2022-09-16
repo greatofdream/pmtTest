3453c48f0076adc5d79ff9b0f749137148cad81a
greatofdream <greatofdream@gmail.com>
2022-09-16 15:11:01 +0800

    store Dark Noise result

--- storePMT.py
+++ storePMT.py
@@ -15,9 +15,21 @@ psr.add_argument('--laser', help='laser result')
 args = psr.parse_args()
 with h5py.File(args.laser, 'r') as ipt:
     pulse = ipt['mergepulse'][:]
+    laserres = ipt['merge'][:]
+try:
+    with h5py.File(args.dark, 'r') as ipt:
+        darkres = ipt['merge'][:]
+    darkExpect = True
+except:
+    darkExpect = False
 storecsv = pd.read_csv(args.opt)
 tmpcsv = storecsv.set_index('PMT')
-tmpcsv.loc[args.pmt, 'Pre'] = pulse[3]
-tmpcsv.loc[args.pmt, 'After1'] = pulse[4]
-tmpcsv.loc[args.pmt, 'After2'] = pulse[5]
+tmpcsv.loc[args.pmt, ['Gain', 'PV', 'Res', 'TTS', 'Rise', 'Fall', 'TH', 'FWHM']] = (
+    laserres[0]['Gain'], laserres[0]['PV'], laserres[0]['Res'], laserres[0]['TTS'], laserres[0]['Rise'], laserres[0]['Fall'], laserres[0]['TH'], laserres[0]['FWHM']
+)
+tmpcsv.loc[args.pmt, ['Pre', 'After1', 'After2']] = (pulse[0]['promptWODCR'], pulse[0]['delay1WODCR'], pulse[0]['delay10WODCR'])
+if darkExpect:
+    tmpcsv.loc[args.pmt, ['Gain_DR', 'PV_DR', 'Res_DR', 'DCR', 'Rise_DR', 'Fall_DR', 'TH_DR', 'FWHM_DR']] = (
+        darkres[0]['Gain'], darkres[0]['PV'], darkres[0]['Res'], darkres[0]['DCR'], darkres[0]['Rise'], darkres[0]['Fall'], darkres[0]['TH'], darkres[0]['FWHM']
+    )
 tmpcsv.sort_index().reset_index().to_csv(args.opt, index=False)
