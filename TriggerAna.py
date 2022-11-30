e5010bc05ccd5dfdd6f336d17d069c005633847e
greatofdream <greatofdream@gmail.com>
2022-11-30 12:43:54 +0000

    count in three windows

--- TriggerAna.py
+++ TriggerAna.py
@@ -13,27 +13,28 @@ delay1B = 300
 delay1E = 1000
 delay10B = 1000
 delay10E = 10000
-def Pulse(wave, base, std, up10, down10, promptB,promptE,delay1B,delay1E,delay10B,delay10E):
-    before = wave[max(0,up10-promptB):(up10-promptE)]
-    after_1 = wave[(down10+delay1B):min(down10+delay1E,wave.shape[0])]
-    after_10 = wave[(down10+delay10B):min(down10+delay10E,wave.shape[0])]
-    before_pulse = -1
-    after_1_pulse = -1
-    after_10_pulse = -1
-    before_pulse_H,after_1_pulse_H, after_10_pulse_H = 0, 0, 0
-    threshold = 5*std
-    if threshold<3:
-        threshold = 3
-    if min(before)<=(base-threshold):
-        before_pulse = np.argmin(before)+max(0,up10-promptB)
-        before_pulse_H = -np.min(before)
-    if min(after_1)<=(base-threshold):
-        after_1_pulse = np.argmin(after_1)+(down10+delay1B)
-        after_1_pulse_H = -np.min(after_1)
-    if min(after_10)<=(base-threshold):
-        after_10_pulse = np.argmin(after_10)+(down10+delay10B)
-        after_10_pulse_H = -np.min(after_10)
-    return before_pulse, after_1_pulse, after_10_pulse, before_pulse_H,after_1_pulse_H, after_10_pulse_H
+def Pulse(wave, start, end, threshold, intLimit):
+    minIndex = start + np.argmin(wave[start:end])
+    minpeak = -wave[minIndex]
+    integral_b = minIndex-config.baselength
+    integral_e = min((minIndex+config.afterlength), intLimit)
+    minpeakcharge = -np.sum(wave[integral_b:integral_e])
+    isTrigger = True
+    if minpeak>threshold:
+        # rising edge 0.1 down edge 0.1
+        up10, up50, up90 = Qb(wave, minIndex, 0)
+        if np.min(wave[int(up10):minIndex]) < -minpeak:
+            # The minindex is the falling edge, omit
+            isTrigger = False
+            up10, up50, up90 = minIndex, minIndex, minIndex
+            down10, down50, down90 = minIndex, minIndex, minIndex
+        else:
+            down10, down50, down90 = Qe(wave, minIndex, 0)
+    else:
+        isTrigger = False
+        up10, up50, up90 = minIndex, minIndex, minIndex
+        down10, down50, down90 = minIndex, minIndex, minIndex
+    return isTrigger, up10, up50, up90, down10, down50, down90, minpeak, minIndex, minpeakcharge
 if __name__=="__main__":
     psr = argparse.ArgumentParser()
     psr.add_argument('-i', dest='ipt', help='input root file')
@@ -44,7 +45,7 @@ if __name__=="__main__":
     psr.add_argument('-n', dest='nchannel', type=int, default=8, help='number of channel')
     psr.add_argument('--interval', help='the time interval for each channel')
     psr.add_argument('--result', help='result of preanalysis')
-    psr.add_argument('--QE', default=False, action='store_true')
+    psr.add_argument('--QE', default=False, action='store_true', help='consider the laser pulse and late pulse')
     args = psr.parse_args()
 
     triggerch = args.trigger
@@ -57,9 +58,7 @@ if __name__=="__main__":
     channels = [int(c) for c in args.channel]
     # read the time interval
     with h5py.File(args.interval, 'r') as ipt:
-        interval = ipt['interval'][:]
-        if args.QE:
-            rinterval = ipt['rinterval'][:]
+        rinterval = ipt['rinterval'][:]
     # read the analysis result
     info = []
     with h5py.File(args.result, 'r') as ipt:
@@ -70,13 +69,16 @@ if __name__=="__main__":
     if not args.QE:
         # 计算前后脉冲:initial the result
         pulse = []
-        storedtype = [('EventID', '<i4'), ('isTrigger', bool), ('promptPulse','<f4'), ('DelayPulse1','<f4'), ('DelayPulse10','<f4'), ('promptPulseH','<f4'), ('DelayPulse1H','<f4'), ('DelayPulse10H','<f4'), ('up10', '<f4'), ('down10', '<f4')]
+        storedtype = [('EventID', '<i4'), ('isTrigger', bool), ('begin10', '<f4'), ('down10', '<f4'), ('begin50', '<f4'), ('down50', '<f4'), ('begin90', '<f4'), ('down90', '<f4'),
+        ('minPeak', '<f4'), ('minPeakPos', '<i4'), ('minPeakCharge', '<f4')]
         for i in range(len(args.channel)):
-            pulse.append(np.zeros((entries,),dtype=storedtype))
-
+            # 1,2,3 columns are laser, main, late pulse. [laserB, laserE], [laserE, -laserE], [elasticB, elasticE]
+            pulse.append(np.zeros((entries, 3),dtype=storedtype))
+        intervalCenters = rinterval['mean']
         for i, (wave, eid, ch) in enumerate(zip(waveforms, eventIds, channelIds)):
             wave = wave.reshape((ch.shape[0],-1))
             chmap = Series(range(ch.shape[0]), index=ch)
+            triggerTime = trigger['triggerTime'][i]
             for j in range(len(args.channel)):
                 w = wave[chmap.loc[channels[j]]]
                 anar = info[j][i]
@@ -85,22 +87,18 @@ if __name__=="__main__":
                 threshold = args.nsigma*std
                 if threshold<3:
                     threshold = 3
-                interval_j = [int(ti) for ti in interval[j][['start', 'end']]]
-                promptPulse, delayPulse1, delayPulse10 = -1, -1, -1
-                promptPulseH, delayPulse1H, delayPulse10H = 0, 0, 0
-                # Baseline inteval_j
-                if np.max(baseline - w[interval_j[0]:interval_j[1]])>threshold:
-                    minIndex = interval_j[0] + np.argmin(w[interval_j[0]:interval_j[1]])
-                    # rising edge 0.1 down edge 0.1
-                    up10, _, _ = Qb(w-baseline, minIndex, 0)
-                    down10, _, _ = Qe(w-baseline, minIndex, 0)
-                    # TODO:baseline-3*std
-                    # TODO:0.3-1us. 1-10us.
-                    #check if pos_histmax_'ch{}'.format(args.channel[j]) is right.it means an int number 
-                    promptPulse, delayPulse1, delayPulse10, promptPulseH, delayPulse1H, delayPulse10H = Pulse(w-baseline, 0, std, int(up10), int(down10), promptB,promptE,delay1B,delay1E,delay10B,delay10E)
-                    pulse[j][i] = (eid, True, promptPulse, delayPulse1, delayPulse10, promptPulseH, delayPulse1H, delayPulse10H, up10, down10)
+                # late pulse
+                pulse[j][i, 2] = (eid, *Pulse(w-baseline, int(triggerTime + intervalCenters[j] + config.elasticB), int(triggerTime + intervalCenters[j] + config.elasticE), threshold, w.shape[0]))
+                # main pulse
+                if pulse[j][i, 2]['isTrigger']:
+                    intLimit = pulse[j][i, 2]['minPeakPos']
                 else:
-                    pulse[j][i] = (eid, False, promptPulse, delayPulse1, delayPulse10, promptPulseH, delayPulse1H, delayPulse10H, 0, 0)
+                    intLimit = w.shape[0]
+                pulse[j][i, 1] = (eid, *Pulse(w-baseline, int(triggerTime + intervalCenters[j] + config.laserE/2), int(triggerTime + intervalCenters[j] - config.laserE/2), threshold, intLimit))
+                # laser pulse
+                if pulse[j][i, 1]['isTrigger']:
+                    intLimit = pulse[j][i, 1]['minPeakPos']
+                pulse[j][i, 0] = (eid, *Pulse(w-baseline, int(triggerTime + intervalCenters[j] + config.laserB), int(triggerTime + intervalCenters[j] + config.laserE), threshold, intLimit))
     else:
         # 计算
         waveana = Triggerana()
