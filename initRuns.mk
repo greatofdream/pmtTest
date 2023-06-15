114a5392f0c2dcde4bc087000a19a34192aee2da
aiqiang <greatofdream@gmail.com>
2023-06-15 22:32:24 +0900

    initRuns script

---
+++ initRuns.mk
@@ -0,0 +1,23 @@
+.PHONY: all sk6 sk7
+all: $(phase1)_$(phase2).csv sk6_summary.csv sk7_summary.csv
+sk6:=/disk02/lowe8/sk6/lin/lin.0
+sk7:=/disk02/lowe10/sk7/lin/lin.0
+compare: $(runs:%=$(state)/%.png)
+# echo $(foreach runTuple,$(runTuples),$(word 1,$(subst _, ,$(runTuple))))
+$(state)/%.png: 
+sk6_summary.csv:
+	echo -n > $@
+	for i in {86119..86161};do echo $$i; python3 initRuns.py $$i >> $@; done
+sk7_summary.csv:
+	echo -n > $@
+	for i in {91356..91437};do echo $$i; python3 initRuns.py $$i >> $@; done
+$(phase1)_$(phase2).csv: $(phase1)_summary.csv $(phase2)_summary.csv
+	python3 initSetting.py -i $^ --label $(phase1) $(phase2) -o $@
+define runtuple
+result/$(1)_$(2).h5: $$($(phase1))$(1).root $$($(phase2))$(2).root
+	mkdir -p $$(dir $$@)
+	python3 compare.py $$^ -o $$@
+
+endef
+
+$(eval $(foreach runTuple,$(runTuples),$(call runtuple,$(word 1,$(subst _, ,$(runTuple))),$(word 2,$(subst _, ,$(runTuple))))))
---
+++ initRuns.py
@@ -0,0 +1,38 @@
+import argparse, os, re, pandas as pd
+from datetime import datetime
+# def getSetting(summary_files):
+def getXZE(comment):
+    x, z, E = 0, 0, 0
+    result = re.match(r'.+x=?([-|+]?\d+)m?,? ?z=?([-|+]?\d+)m?,? +(\d+) ?mev', comment, re.I)
+    if result:
+        x, z, E = result.group(1), result.group(2), result.group(3)
+    else:
+        result = re.match(r'.+(\d+) ?mev,? ?x=?([-|+]?\d+)m?,? ?z=?([-|+]?\d+)m?', comment, re.I)
+        if result:
+            x, z, E = result.group(2), result.group(3), result.group(1)
+        else:
+            result = re.match(r'.+\(([-|+]?\d+)m?,? ?([-|+]?\d+)m?\),? +(\d+) ?mev', comment, re.I)
+            if result:
+                x, z, E = result.group(1), result.group(2), result.group(3)
+    return x, z, E
+
+if __name__=="__main__":
+    psr = argparse.ArgumentParser()
+    psr.add_argument('ipt', help='run no')
+    args = psr.parse_args()
+    response = os.popen('summary {}'.format(args.ipt))
+    summary = {}
+    for res in response:
+        kv = [i.strip() for i in res.split('=', 1)]
+        if len(kv)==2:
+            summary[kv[0]] = kv[1]
+    # print(summary)
+    mode = 0
+    if summary['Run Mode']=='10:LINAC' and (not re.search('(retune|error)', summary['End Comment'], re.I)):
+        deltatime = (datetime.strptime(summary['End time'], "%a %b %d %H:%M:%S %Y") - datetime.strptime(summary['Start time'], "%a %b %d %H:%M:%S %Y")).total_seconds()/60
+    # if summary['Run Mode']=='10:LINAC' and not (re.search('MW', summary['Comment'], 0) or re.search('micro', summary['Comment'], 1) or re.search('[retune|error]', summary['End Comment'], 1)):
+        if deltatime > 5:
+            if re.search('(mw|micro)', summary['Comment'], re.I):
+                mode = 1
+            x, z, E = getXZE(summary['Comment'])
+            print('{},{},{},{},{},{},{}'.format(summary['Run number'], x, z, E, mode, int(deltatime), summary['End Comment'].replace(',', ' ')))
