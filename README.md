d20e767507bc08c42edb97e9204dd12406b24ae5
aiqiang <greatofdream@gmail.com>
2023-06-23 23:28:55 +0900

    update the runno for sk7

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ make sk7_summary.csv -f initRuns.mk
   + `86157`: `X=12m`->`X=-12m`
 ```
 make phase1=sk6 phase2=sk7 -f initRuns.mk sk6_sk7.csv
+make phase1=sk6 phase2=sk7 -f initRuns.mk sk6_sk7_ZE.csv
 ```
 
 ```
@@ -14,5 +15,6 @@ python3 ana.py
 ```
 
 ```
-python3 compare.py
+python3 compare.py -i sk6_sk7_ZE.csv
+python3 compare.py -i sk6_sk6.csv --onebyone
 ```
--- initRuns.mk
+++ initRuns.mk
@@ -10,9 +10,12 @@ sk6_summary.csv:
 	for i in {86119..86161};do echo $$i; python3 initRuns.py $$i >> $@; done
 sk7_summary.csv:
 	echo -n > $@
-	for i in {91356..91437};do echo $$i; python3 initRuns.py $$i >> $@; done
+	for i in {91356..91563};do echo $$i; python3 initRuns.py $$i >> $@; done
 $(phase1)_$(phase2).csv: $(phase1)_summary.csv $(phase2)_summary.csv
 	python3 initSetting.py -i $^ --label $(phase1) $(phase2) -o $@
+$(phase1)_$(phase2)_ZE.csv: $(phase1)_summary.csv $(phase2)_summary.csv
+	python3 initSetting.py -i $^ --label $(phase1) $(phase2) --on Z E --how outer -o $@
+
 define runtuple
 result/$(1)_$(2).h5: $$($(phase1))$(1).root $$($(phase2))$(2).root
 	mkdir -p $$(dir $$@)
