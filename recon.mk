a5d9af55981a403352b372b356b0c084030ef823
aiqiang <greatofdream@gmail.com>
2023-08-13 19:17:12 +0900

    recon scripts with data parameter

--- recon.mk
+++ recon.mk
@@ -9,6 +9,8 @@ MCDATA:=$(wildcard $(phasedir)/*.root)
 tentativeFile:=$(if $(tentative),tentative_$(phase).txt,)
 tentativeArg:=$(if $(tentative),--tentative,,)
 tentativeFlag:=$(if $(tentative),_tentative,,)
+linacDat:=./$(phase)_linac_runsum.dat
+geDat:=./$(phase)_ge_runsum.dat
 all:
 	mkdir -p recon/$(phase)
 	python3 submit_recon_data.py --summary $(summaryfile) --phase $(phase) -o recon/$(phase) $(tentativeArg)
@@ -18,11 +20,11 @@ allskg4:
 RECON: $(foreach i,$(RAWDATA),recon/$(phase)/$(run)/$(subst .all,,$(subst rfm_lin$(run).,,$(notdir $i))))
 recon/$(phase)/$(run)/%.root: $(phasedir)/rfm_lin$(run).%.all.root
 	mkdir -p $(dir $@)
-	$(CONDOR) recon_src/lowfit_data$(tentativeFlag) $(run) $@ $^ $(tentative) $(tentativeFile)
+	$(CONDOR) recon_src/lowfit_data$(tentativeFlag) $(run) $@ $^ $(linacDat) $(geDat) $(tentative) $(tentativeFile)
 RECONMC: $(foreach i,$(MCDATA),reconskg4/$(phase)/$(run)/$(notdir $i))
 reconskg4/$(phase)/$(run)/%.root: $(phasedir)/%.root
 	mkdir -p $(dir $@)
-	$(CONDOR) recon_src/lowfit_mc$(tentativeFlag) $(run) $@ $^ $(tentative) $(tentativeFile)
+	$(CONDOR) recon_src/lowfit_mc$(tentativeFlag) $(run) $@ $^ $(linacDat) $(geDat) $(tentative) $(tentativeFile)
 
 # Delete partial files when the processes are killed.
 .DELETE_ON_ERROR:
